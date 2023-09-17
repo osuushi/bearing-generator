b3fed470f9c364f404456eea92f5ce77f8232189
Ada Cohen <ada@fieldbook.com>
2023-09-17 00:25:06 -0400

    Refactor and add generator fo 2 ring bearing

---
+++ src/bearing-3-ring.ts
@@ -0,0 +1,299 @@
+import { Polygon } from "./polygon";
+import { revolve } from "./revolve";
+import { newXZPoint, Point, Triangle } from "./types";
+
+const retentionRadiusOffset = 1;
+const retentionVerticalOffset = 0.5;
+const retentionTiltOffset = 2;
+const minThickness = 1.25;
+
+export class Bearing3Ring {
+  outerDiameter: number;
+  boreDiameter: number;
+  width: number;
+  clearance: number;
+
+  constructor({
+    outerDiameter,
+    boreDiameter,
+    width,
+    clearance,
+  }: {
+    outerDiameter: number;
+    boreDiameter: number;
+    width: number;
+    clearance: number;
+  }) {
+    this.outerDiameter = outerDiameter;
+    this.boreDiameter = boreDiameter;
+    this.width = width;
+    this.clearance = clearance;
+  }
+
+  get sliderInnerDiameter(): number {
+    return this.boreDiameter + (this.outerDiameter - this.boreDiameter) / 3;
+  }
+
+  get sliderOuterDiameter(): number {
+    return (
+      this.boreDiameter + (2 * (this.outerDiameter - this.boreDiameter)) / 3
+    );
+  }
+
+  getTriangles(): Triangle[] {
+    return [
+      ...this.getTrianglesForInnerRing(),
+      ...this.getTrianglesForSlider(),
+      ...this.getTrianglesForOuterRing(),
+    ];
+  }
+
+  trianglesWithMirroredBottom(bottomTriangles: Triangle[]): Triangle[] {
+    const topTriangles = bottomTriangles.map((t) => {
+      t = t.flip();
+      t.a.z = this.width - t.a.z;
+      t.b.z = this.width - t.b.z;
+      t.c.z = this.width - t.c.z;
+      return t;
+    });
+    return [...bottomTriangles, ...topTriangles];
+  }
+
+  getTrianglesForInnerRing(): Triangle[] {
+    return revolve(this.innerRingPolygon);
+  }
+
+  get innerRingPolygon(): Polygon {
+    let innerRadius = this.boreDiameter / 2;
+    let outerMax = this.sliderInnerDiameter / 2 - this.clearance;
+    let outerMin = outerMax - retentionRadiusOffset;
+    return new Polygon([
+      // Bottom
+      newXZPoint(innerRadius, 0),
+      newXZPoint(outerMax, 0),
+      // Rise up to the top of the bottom tilt
+      newXZPoint(outerMax, retentionVerticalOffset),
+      // Tilt in
+      newXZPoint(outerMin, retentionVerticalOffset + retentionTiltOffset),
+      // Rise to the top tilt
+      newXZPoint(
+        outerMin,
+        this.width - retentionVerticalOffset - retentionTiltOffset
+      ),
+      // Tilt out
+      newXZPoint(outerMax, this.width - retentionVerticalOffset),
+      // Rise to the top
+      newXZPoint(outerMax, this.width),
+      // Top
+      newXZPoint(innerRadius, this.width),
+      // Polygon closed implicitly
+    ]);
+  }
+
+  getTrianglesForSlider(): Triangle[] {
+    return revolve(this.sliderPolygon);
+  }
+
+  get sliderPolygon(): Polygon {
+    const innerMax = this.sliderInnerDiameter / 2;
+    const innerMin = innerMax - retentionRadiusOffset;
+    const outerMin = this.sliderOuterDiameter / 2;
+    const outerMax = outerMin + retentionRadiusOffset;
+
+    return new Polygon([
+      // Bottom
+      newXZPoint(innerMax, 0),
+      newXZPoint(outerMin, 0),
+      // Rise up to the top of the bottom tilt
+      newXZPoint(outerMin, retentionVerticalOffset),
+      // Tilt out
+      newXZPoint(outerMax, retentionVerticalOffset + retentionTiltOffset),
+      // Rise to the top tilt
+      newXZPoint(
+        outerMax,
+        this.width - retentionVerticalOffset - retentionTiltOffset
+      ),
+      // Tilt in
+      newXZPoint(outerMin, this.width - retentionVerticalOffset),
+      // Rise to the top
+      newXZPoint(outerMin, this.width),
+      // Top
+      newXZPoint(innerMax, this.width),
+      // Drop to the top tilt
+      newXZPoint(innerMax, this.width - retentionVerticalOffset),
+      // Tilt in
+      newXZPoint(
+        innerMin,
+        this.width - retentionVerticalOffset - retentionTiltOffset
+      ),
+      // Drop to the bottom tilt
+      newXZPoint(innerMin, retentionVerticalOffset + retentionTiltOffset),
+      // Tilt out
+      newXZPoint(innerMax, retentionVerticalOffset),
+      // Polygon closed implicitly
+    ]);
+  }
+
+  getTrianglesForOuterRing(): Triangle[] {
+    return revolve(this.outerRingPolygon);
+  }
+
+  get outerRingPolygon(): Polygon {
+    const innerMin = this.sliderOuterDiameter / 2 + this.clearance;
+    const innerMax = innerMin + retentionRadiusOffset;
+
+    return new Polygon([
+      // Outer edge
+      newXZPoint(this.outerDiameter / 2, 0),
+      newXZPoint(this.outerDiameter / 2, this.width),
+      // Top
+      newXZPoint(innerMin, this.width),
+      // Drop down by the vertical offset
+      newXZPoint(innerMin, this.width - retentionVerticalOffset),
+      // Tilt in by the tilt offset
+      newXZPoint(
+        innerMax,
+        this.width - retentionVerticalOffset - retentionTiltOffset
+      ),
+      // Drop down to the top of the bottom tilt
+      newXZPoint(innerMax, retentionVerticalOffset + retentionTiltOffset),
+      // Tilt out
+      newXZPoint(innerMin, retentionVerticalOffset),
+      // Drop to X axis
+      newXZPoint(innerMin, 0),
+      // Bottom is implied by closing the polygon
+    ]);
+  }
+
+  drawPreview(canvas: HTMLCanvasElement) {
+    // Get all the polygons
+    // Get the extents of the points in the polygons
+    // Get the bounds of the canvas
+    // Compute the scale factor so that the largest ratio of polygon dimension to canvas dimension is 0.8
+    // Compute the translation so that the center of the polygons is at the center of the canvas
+    // Transform the polygons
+    // Clear the canvas
+    // Draw a rectangle of the polygon bounds to fake the insides of the bearing rings
+    // Draw the polygons
+    // Mirror the polygons over the Y axis and draw them again.
+
+    let innerRing = this.innerRingPolygon;
+    let slider = this.sliderPolygon;
+    let outerRing = this.outerRingPolygon;
+    const polygons = [innerRing, slider, outerRing];
+
+    const maxX = Math.max(
+      ...polygons.map((p) =>
+        p.points.reduce((max, p) => Math.max(max, p.x), -Infinity)
+      )
+    );
+    const minX = -maxX;
+    const minY = Math.min(
+      ...polygons.map((p) =>
+        p.points.reduce((min, p) => Math.min(min, p.z), Infinity)
+      )
+    );
+    const maxY = Math.max(
+      ...polygons.map((p) =>
+        p.points.reduce((max, p) => Math.max(max, p.z), -Infinity)
+      )
+    );
+
+    const canvasWidth = canvas.width;
+    const canvasHeight = canvas.height;
+
+    const scaleX = canvasWidth / (maxX - minX);
+    const scaleY = canvasHeight / (maxY - minY);
+    const scale = Math.min(scaleX, scaleY) * 0.8;
+    const translateX = canvasWidth / 2 - ((maxX + minX) / 2) * scale;
+    const translateY = canvasHeight / 2 - ((maxY + minY) / 2) * scale;
+
+    const transform = (p: Point) => {
+      return newXZPoint(p.x * scale + translateX, p.z * scale + translateY);
+    };
+
+    const ctx = canvas.getContext("2d")!;
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    const leftPoint = transform(newXZPoint(-this.boreDiameter / 2, 0));
+    const rightPoint = transform(newXZPoint(this.boreDiameter / 2, 0));
+    const bgGradient = ctx.createLinearGradient(
+      leftPoint.x,
+      0,
+      rightPoint.x,
+      0
+    );
+    const lightColor = "rgba(206, 207, 176, 1)";
+    const shadeColor = "rgba(102, 102, 69, 1)";
+    bgGradient.addColorStop(0, shadeColor);
+    bgGradient.addColorStop(0.45, lightColor);
+    bgGradient.addColorStop(0.55, lightColor);
+    bgGradient.addColorStop(1, shadeColor);
+    ctx.fillStyle = bgGradient;
+    // Draw the background rectangle
+    ctx.fillRect(
+      transform(newXZPoint(minX, minY)).x,
+      transform(newXZPoint(minX, minY)).z,
+      transform(newXZPoint(maxX, maxY)).x - transform(newXZPoint(minX, minY)).x,
+      transform(newXZPoint(maxX, maxY)).z - transform(newXZPoint(minX, minY)).z
+    );
+    const upperLeftCorner = transform(newXZPoint(minX, minY));
+    const lowerRightCorner = transform(newXZPoint(maxX, maxY));
+
+    const fgGradient = ctx.createLinearGradient(
+      upperLeftCorner.x,
+      upperLeftCorner.z,
+      lowerRightCorner.x,
+      upperLeftCorner.z
+    );
+    fgGradient.addColorStop(0, "rgba(255, 200, 90, 1)");
+    fgGradient.addColorStop(0.5, "rgba(240, 229, 200, 1)");
+    fgGradient.addColorStop(1, "rgba(226, 194, 100, 1)");
+    ctx.fillStyle = fgGradient;
+
+    for (const polygon of polygons) {
+      const points = polygon.points.map(transform);
+      ctx.beginPath();
+      ctx.moveTo(points[0].x, points[0].z);
+      for (const point of points.slice(1)) {
+        ctx.lineTo(point.x, point.z);
+      }
+      ctx.closePath();
+      ctx.fill();
+    }
+
+    for (const polygon of polygons) {
+      const points = polygon.points.map((p) => {
+        const transformed = transform(p);
+        return newXZPoint(canvasWidth - transformed.x, transformed.z);
+      });
+      ctx.beginPath();
+      ctx.moveTo(points[0].x, points[0].z);
+      for (const point of points.slice(1)) {
+        ctx.lineTo(point.x, point.z);
+      }
+      ctx.closePath();
+      ctx.fill();
+    }
+  }
+
+  get fileName(): string {
+    // Compute a nice file name from the specs
+    const formatNumber = (n: number) => n.toFixed(2).replace(/\.?0*$/, "");
+    return `bearing-3-ring-${formatNumber(this.boreDiameter)}-${formatNumber(
+      this.outerDiameter
+    )}-${formatNumber(this.width)}-${formatNumber(this.clearance)}.stl`;
+  }
+
+  validate() {
+    if (
+      this.boreDiameter >=
+      this.sliderInnerDiameter - minThickness * 2 - this.clearance * 2
+    ) {
+      throw new Error("Bore diameter is too large for the outer diameter");
+    }
+  }
+}
--- src/generate.ts
+++ src/generate.ts
@@ -1,54 +1,78 @@
-import { Bearing } from "./bearing"
-import { makeBinarySTL } from "./stl"
+import { Bearing } from "./bearing";
+import { Bearing2Ring } from "./bearing-2-ring";
+import { Bearing3Ring } from "./bearing-3-ring";
+import { makeBinarySTL } from "./stl";
 
 export function generate() {
   // Get the bearing
-  let bearing = bearingFromForm()
+  let bearing = bearingFromForm();
   // Get the triangles
-  let triangles = bearing.getTriangles()
+  let triangles = bearing.getTriangles();
   // Create the STL contents
-  const stl = makeBinarySTL(triangles)
+  const stl = makeBinarySTL(triangles);
 
   // Create a link to download the STL
-  let link = document.createElement('a')
-  link.download = bearing.fileName
+  let link = document.createElement("a");
+  link.download = bearing.fileName;
   // We have a uint8array, but we need a downloadable url. We can do this by creating a blob.
-  let blob = new Blob([stl], { type: 'application/octet-stream' })
-  link.href = URL.createObjectURL(blob)
-  link.click()
+  let blob = new Blob([stl], { type: "application/octet-stream" });
+  link.href = URL.createObjectURL(blob);
+  link.click();
 }
 
 export function generatePreview() {
-  let bearing = bearingFromForm()
-  let canvas = document.querySelector<HTMLCanvasElement>('#preview')!
-  bearing.drawPreview(canvas)
+  let bearing = bearingFromForm();
+  let canvas = document.querySelector<HTMLCanvasElement>("#preview")!;
+  bearing.drawPreview(canvas);
 }
 
 export function validate() {
-  let bearing = bearingFromForm()
+  let bearing = bearingFromForm();
   try {
-    bearing.validate()
-    document.querySelector('#error')!.textContent = ''
+    bearing.validate();
+    document.querySelector("#error")!.textContent = "";
   } catch (e) {
     if (e instanceof Error) {
-      document.querySelector('#error')!.textContent = e.message
+      document.querySelector("#error")!.textContent = e.message;
     } else {
-      document.querySelector('#error')!.textContent = 'Unknown error'
+      document.querySelector("#error")!.textContent = "Unknown error";
     }
   }
 }
 
 function bearingFromForm(): Bearing {
   // Get the form values
-  let outerDiameter = parseFloat(document.querySelector<HTMLInputElement>('#outer-diameter')!.value)
-  let boreDiameter = parseFloat(document.querySelector<HTMLInputElement>('#bore-diameter')!.value)
-  let width = parseFloat(document.querySelector<HTMLInputElement>('#width')!.value)
-  let clearance = parseFloat(document.querySelector<HTMLInputElement>('#clearance')!.value)
-  // Create the bearing
-  return new Bearing({
-    outerDiameter: outerDiameter,
-    boreDiameter: boreDiameter,
-    width: width,
-    clearance: clearance,
-  })
+  let rings = parseInt(
+    document.querySelector<HTMLInputElement>(
+      'input[name="number-of-rings"]:checked'
+    )!.value
+  );
+  let outerDiameter = parseFloat(
+    document.querySelector<HTMLInputElement>("#outer-diameter")!.value
+  );
+  let boreDiameter = parseFloat(
+    document.querySelector<HTMLInputElement>("#bore-diameter")!.value
+  );
+  let width = parseFloat(
+    document.querySelector<HTMLInputElement>("#width")!.value
+  );
+  let clearance = parseFloat(
+    document.querySelector<HTMLInputElement>("#clearance")!.value
+  );
+  if (rings === 2) {
+    return new Bearing2Ring({
+      outerDiameter: outerDiameter,
+      boreDiameter: boreDiameter,
+      width: width,
+      clearance: clearance,
+    });
+  } else {
+    // Create the bearing
+    return new Bearing3Ring({
+      outerDiameter: outerDiameter,
+      boreDiameter: boreDiameter,
+      width: width,
+      clearance: clearance,
+    });
+  }
 }
